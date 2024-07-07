f0a27d643776c6672d3ce458c4cc3ea4f3bd5602
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-07-07 14:58:42 +0500

    update

--- MernProject/src/pages/ArticleList.jsx
+++ MernProject/src/pages/ArticleList.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import articleContent from "../../utils/Content";
 import { Link } from "react-router-dom";
+// import pic from "../assets/pic1.jpg"
 function ArticleList() {
   return (
     <div>
       <h1>My Articles</h1>
       {articleContent.map((article, index) => (
-
         <div className="container p-4 h-40 border md:w-1/2 " key={index}>
           <div className="">
           <Link to={`/Services/${article.name}`}>
             <img src={article.thumbnail} alt="articlepics" className="md:h-36 w-full object-cover object-center" />
           </Link>
-
           </div>
         </div>
-        
-
       ))}
+      <img src="../assets/pic1.jpg" alt=" new pic" />
     </div>
   );
 }
