c9f3aa96a32463709c7f9e56dfa1a6d61fa466ee
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-07-09 16:16:18 +0500

    update

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ app.use(express.json({extended:false}))
 // route for db connection 
 app.get('/api/articles/:name', async (req,res)=>{
     try{
+        console.log('check!');
         const articleName = req.params.name;
-        const client = await MongoClient.connect('mongodb://localhost:27017');
+        console.log(articleName);
+
+        const client = await MongoClient.connect('mongodb://localhost:27017/');
         const db = client.db('merndb');
         const articleInfo = await db.collection('articles').findOne({name:articleName});
-        res.status(200).json(articleInfo);
+        console.log('check2!');
+
+         res.status(200).json(articleInfo);
         client.close();
     }catch(err){
         res.status(500).json({message:'Error connecting to db by saddam',err})
